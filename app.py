6a091386459d5a147d309dffb7cf8961b3e1cb07
Abhishek Meena <118628637+abhishekmeena33@users.noreply.github.com>
2024-07-09 00:32:19 +0530

    Added Video as a Background

--- app.py
+++ app.py
@@ -4,6 +4,40 @@ import pickle
 import requests
 
 
+st.set_page_config(layout="wide")
+
+video_html = """
+		<style>
+
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
+
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://drive.google.com/file/d/1H6xQKGvIThy-7LLjC2Ipal9_wF_Z4biu/view?usp=drive_link">
+		  Your browser does not support HTML5 video.
+		</video>
+        """
+
+st.markdown(video_html, unsafe_allow_html=True)
+st.title('Video page')
+
+st.markdown("This text is written on top of the background video! 😁")
+
 def fetch_poster(movie_id):
     response = requests.get(
         'https://api.themoviedb.org/3/movie/{}?api_key=a89bd48535fad9d4ec39ea960111a56b&language=en-US%27'.format(
