127848a6333979b10025fe58635003ba57151f14
Abhishek Meena <118628637+abhishekmeena33@users.noreply.github.com>
2024-08-04 23:52:33 +0530

    Major Changes in layout
    Now dropdown for details of the object, added Overview and Ratings of the movies, changed background to a cool gif

--- app.py
+++ app.py
@@ -2,87 +2,80 @@ import streamlit as st
 import pandas as pd
 import pickle
 import requests
+from streamlit_lottie import st_lottie
 
-
-
-def fetch_poster(movie_id):
+# Function to fetch poster and movie details
+def fetch_movie_details(movie_id):
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=a89bd48535fad9d4ec39ea960111a56b&language=en-US%27'.format(
-            movie_id))
-
+        f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=a89bd48535fad9d4ec39ea960111a56b&language=en-US')
     data = response.json()
+    poster_path = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    overview = data['overview']
+    rating = data['vote_average']
+    return poster_path, overview, rating
 
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-
-
+@st.cache_data
 def recommend_movies(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
-    recommended_movies_posters = []
+    recommended_movies_details = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-
-        #fetching poster
         recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_movies_posters.append(fetch_poster(movie_id))
+        details = fetch_movie_details(movie_id)
+        recommended_movies_details.append(details)
 
-    return recommended_movies, recommended_movies_posters
+    return recommended_movies, recommended_movies_details
 
+# Function to load Lottie animation from URL
+def load_lottieurl(url: str):
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    return response.json()
 
+# Load data
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-#Background-Image with css
-
+# Background-Image with CSS
 background_image = """
 <style>
 [data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://www.pixel4k.com/wp-content/uploads/2019/03/colorful-blur-4k_1551645486.jpg.webp");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
+    background-image: url("https://wallpapercave.com/wp/wp2757874.gif");
+    background-size: cover;
+    background-position: center;
     background-repeat: no-repeat;
 }
 </style>
 """
-
 st.markdown(background_image, unsafe_allow_html=True)
 
-st.title('Movie Recommender Buddy')
+# Load Lottie animations
+# lottie_url = "https://lottie.host/5f2dad6b-6646-4c16-bf5d-410b92f227c9/aP0UjbCIXF.json"  # Example URL
+# lottie_animation = load_lottieurl(lottie_url)
+
+st.title('Movie Recommender Buddy🎬')
+# st_lottie(lottie_animation, height=200, key="header_animation")
 
 selected_movie_name = st.selectbox(
     "Which movie have you watched?",
     movies['title'].values)
 
-#st.button("Reset", type="primary")
-if st.button("Recommend") or st.text_input:
-    recommendations, posters = recommend_movies(selected_movie_name)
+# Recommend button and display results
+if st.button("Recommend") and selected_movie_name:
+    recommendations, details = recommend_movies(selected_movie_name)
 
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommendations[0])
-        st.markdown(f'<a href="https://www.google.com/search?q={recommendations[0]}+movie"><img src="{posters[0]}" width="100"></a>',unsafe_allow_html=True)
+    for i, (movie, detail) in enumerate(zip(recommendations, details)):
+        st.subheader(movie)
+        poster, overview, rating = detail
+        st.markdown(f'<a href="https://www.google.com/search?q={movie}+movie"><img src="{poster}" width="200"></a>',
+                    unsafe_allow_html=True)
+        with st.expander("More Details", expanded=False):
+            st.write(f"**Rating:** {rating}")
+            st.write(f"**Overview:** {overview}")
 
-    with col2:
-        st.text(recommendations[1])
-        st.markdown(f'<a href="https://www.google.com/search?q={recommendations[1]}+movie"><img src="{posters[1]}" width="100"></a>',unsafe_allow_html=True)
-
-    with col3:
-        st.text(recommendations[2])
-        st.markdown(f'<a href="https://www.google.com/search?q={recommendations[2]}+movie"><img src="{posters[2]}" width="100"></a>',unsafe_allow_html=True)
-
-    with col4:
-        st.text(recommendations[3])
-        st.markdown(f'<a href="https://www.google.com/search?q={recommendations[3]}+movie"><img src="{posters[3]}" width="100"></a>',unsafe_allow_html=True)
-
-    with col5:
-        st.text(recommendations[4])
-        st.markdown(f'<a href="https://www.google.com/search?q={recommendations[4]}+movie"><img src="{posters[4]}" width="100"></a>',unsafe_allow_html=True)
-    # for i in range(5):
-    #     col = st.columns(5)[i]
-    #     with col:
-    #         st.text(recommendations[i])
-    #         st.markdown(f'<a href="https://www.google.com/search?q={recommendations[i]}"><img src="{posters[i]}" width="100"></a>',unsafe_allow_html=True)
